2a3bf9e6873c2a7400d1bc1afcd71cb15e77013b
Paulo Borges <pauloborgesfilho@gmail.com>
2013-04-07 20:42:46 -0300

    Change goto cli to handle unicode
    This way goto can cd and print non ascii paths

--- goto/goto.py
+++ goto/goto.py
@@ -1,28 +1,31 @@
 # coding: utf-8
 import sys
+import locale
 import argparse
-from storage import Storage, LABEL_SIZE
+from storage import Storage, NoOptionError, LABEL_SIZE
 
 
 storage = Storage()
+language, encoding = locale.getdefaultlocale()
 
 
 def format_label(label):
-    return label + (LABEL_SIZE - len(label)) * ' '
+    return label + (LABEL_SIZE - len(label)) * u' '
 
 
 def list_labels():
     labels = storage.get_all()
     for label, path in labels.iteritems():
-        print format_label(label), '\t', path
+        s = u'%s  %s' % (format_label(label), path)
+        print s.encode(encoding)
 
 
 def change_directory(label):
     try:
         path = storage.get(label)
         print '<PATH>'
-        print path
-    except:
+        print path.encode(encoding)
+    except NoOptionError:
         sys.stderr.write('%s is not a valid label.\n' % label)
         sys.exit(1)
 
@@ -37,6 +40,7 @@ def main():
     storage.open_or_create()
 
     if args.label:
-        change_directory(args.label)
+        label = unicode(args.label, encoding)
+        change_directory(label)
     else:
         list_labels()
