402ef431ef835c4b564bdb55f63ff9b03c768988
Paulo Borges <pauloborgesfilho@gmail.com>
2013-04-12 23:58:23 -0300

    Remove unused imports from __init__.py

--- goto/__init__.py
+++ goto/__init__.py
@@ -1,10 +1,5 @@
 # coding: utf-8
-from label import main as label_main
-from goto import main as goto_main
-from storage import Storage
-
 VERSION = (0, 2, 1, 'alpha')
-
 
 def get_version(version=None):
     """Derives a PEP386-compliant version number from VERSION."""
